daeab35e6140c7b4f9d7b252e139a2340afa410a
seung-chan <superfly09@daum.net>
2022-02-02 23:57:06 +0900

    fix: 초기 데이터 저장위치 변경 - 스토리지 => fetch로

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
 import { $, $closest } from "./utils/dom.js";
 import store from "./store/storage.js";
-// step1
-// [O] 에스프레소 메뉴에 새로운 메뉴를 확인 버튼 또는 엔터키 입력으로 추가한다.
-// [O] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
-// [O] 사용자 입력값이 빈 값이라면 추가되지 않는다.
-// [O] 메뉴의 수정 버튼을 눌러 메뉴 이름 수정할 수 있다.
-// [O] 메뉴 수정시 브라우저에서 제공하는 prompt 인터페이스를 활용한다.
-// [O] 메뉴 삭제 버튼을 이용하여 메뉴 삭제할 수 있다.
-// [O] 메뉴 삭제시 브라우저에서 제공하는 confirm 인터페이스를 활용한다.
-// [O] 총 메뉴 갯수를 count하여 상단에 보여준다.
 
-// step2
-// 추가
-// localStorage에도 데이터 저장
-
-// 수정
-// 아이디 값으로 찾고, 그 값을 업데이트 후 로컬스토리지에도 저장
-
-// 삭제
-// 해당 키 값으로 로컬 스토리지에서도 삭제
-
+const BASE_URL = "http://localhost:3000/api";
 function App() {
   this.state = {
     espresso: [],
-    prapuccino: [],
-    blendid: [],
-    tivarna: [],
-    dessert: [],
+    frappuccino: [],
+    blended: [],
+    teavana: [],
+    desert: [],
   };
 
   this.init = () => {
@@ -72,13 +54,31 @@ function App() {
     updateMenuCount();
   };
 
-  const addMenuNameHandler = () => {
+  const addMenuNameHandler = async () => {
     if ($("#menu-name").value === "") return alert("값을 입력하세요");
     const menuValue = $("#menu-name").value;
-    this.state[currentCategory].push({ menu: menuValue });
-    store.setStorage(this.state);
-    render();
-    $("#menu-name").value = "";
+    try {
+      const res1 = await fetch(`${BASE_URL}/category/${currentCategory}/menu`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ name: menuValue }),
+      });
+      const res2 = await res1.json();
+      if (res1.status !== 200 && res2.message) throw Error(res2.message);
+    } catch (e) {
+      alert(`메뉴 추가 에러 : ${e.message}`);
+    }
+    try {
+      const listRes1 = await fetch(
+        `${BASE_URL}/category/${currentCategory}/menu`
+      );
+      const listRes2 = await listRes1.json();
+      this.state[currentCategory] = listRes2;
+      render();
+      $("#menu-name").value = "";
+    } catch (e) {}
   };
   const editMenuHandler = (e) => {
     const $parent = e.target.closest("li");
